45a684c56234800402f892b3fdbc74a175da2fc9
Bala <balashekaran2003@gmail.com>
2024-06-07 21:21:54 +0530

    first commit

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { DemoCard } from '../CardsComponents/DemoCard';
-import { Card2 } from '../CardsComponents/Card2';
+// import { Card2 } from '../CardsComponents/Card2';
 import { CardFooter, Button } from "@material-tailwind/react"
 import Aos from 'aos';
 import ER from '../assets/Quiz.png';
